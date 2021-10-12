9e9a5e9dfa814d5bc269602283cbdbb6b6fd8196
harenasr <lio.ar2@gmail.com>
2021-10-12 01:46:49 -0500

    initializer
    initializer

---
+++ src/main/java/com/entelgy/harenas/HarenasApplication.java
@@ -0,0 +1,13 @@
+package com.entelgy.harenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarenasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entelgy/harenas/HarenasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entelgy.harenas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarenasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
